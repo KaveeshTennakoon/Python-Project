ee917ef39fa3233a13d0bd827496bcbb805f659a
Kaveesh Tennakoon <kaveeshoffical@gmail.com>
2025-05-03 09:14:16 +0530

    Fix refresh token endpoint to use correct decorator

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -121,7 +121,7 @@ def login():
 
 # Get access token using refresh token
 @bp.route("/auth/refresh", methods=["POST"])
-@jwt_required()
+@jwt_required(refresh=True)
 def refresh():
     """Endpoint to refresh token using refresh token in request header"""
     try:
